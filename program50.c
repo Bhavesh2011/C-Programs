7f6dcc15c4c587111bd6e2e53adc2830200fd967
Bhavesh Gaikwad <168211950+Bhavesh2011@users.noreply.github.com>
2026-01-03 17:24:25 +0530

    Add files via upload

---
+++ program50.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int DisplaySum(int iNo)
+{
+    int iDigit = 0;
+    int iSum = 0;
+
+    while(iNo != 0)
+    {
+        iDigit = iNo % 10;
+        iNo = iNo / 10;
+        iSum = iSum + iDigit;
+    }
+    return iSum;
+}
+
+int main()
+{
+   int iValue = 0;
+   int iRet = 0;
+   printf("enter number\n");
+   scanf("%d",&iValue);
+
+    iRet = DisplaySum(iValue);
+
+    printf("%d ",iRet);
+
+   return 0;
+}
